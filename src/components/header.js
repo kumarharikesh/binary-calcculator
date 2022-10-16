2912fb1391ed67ca856028e9f76400bee7b4a4c3
Harikesh Patel <43793294+kumarharikesh@users.noreply.github.com>
2022-10-16 23:25:35 +0530

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import "./header.css";
 
 export default function Header() {
   return (
-    <div>
+    <div style="padding-bottom: 1rem">
       <nav className="navigation container">
         <div className="nav-brand">
           <a
